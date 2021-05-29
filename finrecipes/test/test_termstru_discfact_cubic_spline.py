5b5bba07232a1180bc1f9a770cb53f0c6a56dc05
andromeda164 <66296388+andromeda164@users.noreply.github.com>
2021-05-29 14:39:23 +0200

    test_termstru_discfact_cubic_spline

--- finrecipes/test/test_termstru_discfact_cubic_spline.py
+++ finrecipes/test/test_termstru_discfact_cubic_spline.py
@@ -16,7 +16,17 @@
     literature-ref  --
 *******************************************************"""
 import pytest
+import pandas as pd
 from finrecipes.termstru_discfact_cubic_spline import *
+
+#@pytest.fixture(autouse=True)
+def load_sample_data():
+    df = pd.read_excel(
+        "test_termstru_discfact_cubic_spline.xlsx",
+        usecols=["x","y"],
+        )
+    return df
+
 
 def test_termstru_discfact_cubic_spline():
     
@@ -27,8 +37,9 @@ def test_termstru_discfact_cubic_spline():
 
     def serpentine_curve(x):
         return x/(0.25 + x*x)
-    t = [-1.25,-1.15,-1.05,-0.95,-0.85,-0.75,-0.65,-0.55,-0.45,-0.35,-0.25,
-         -0.15,-0.05,0.05,0.15,0.25,0.35,0.45,0.55,0.65,0.75,0.85,0.95,1.05,1.15,1.25]
+    df = load_sample_data()
+    #x = [-1.25,-1.15,-1.05,-0.95,-0.85,-0.75,-0.65,-0.55,-0.45,-0.35,-0.25,
+    #     -0.15,-0.05,0.05,0.15,0.25,0.35,0.45,0.55,0.65,0.75,0.85,0.95,1.05,1.15,1.25]
     
     y = [-0.689655172414,-0.731319554849,-0.776340110906,-0.824295010846,-0.874035989717, \
          -0.923076923077,-0.966542750929,-0.995475113122,-0.994475138122,-0.939597315436, \
@@ -37,10 +48,10 @@ def test_termstru_discfact_cubic_spline():
          0.874035989717,0.824295010846,0.776340110906,0.731319554849,0.689655172414]
     n = len(t) - 1
     
-    z = spline3_coef(t, y)
+    z = spline3_coef(df['x'],df['y'])
     
-    for i in numpy.arange(-1.25,1.3,0.05):
-        num = spline3_eval(t,y,z,i)
+    for i in df['x']: #numpy.arange(-1.25,1.3,0.05):
+        num = spline3_eval(df['x'],df['y'],z,i)
         analyt = serpentine_curve(i)
         deviationPercent = (analyt - num) / analyt * 100.0
         assert round(deviationPercent, 0) == 0.0
