4eb308e316d19353df905c3208b36f0199f5d349
andromeda164 <66296388+andromeda164@users.noreply.github.com>
2021-05-29 08:33:55 +0200

    further test regarding conftest

--- finrecipes/conftest.py
+++ finrecipes/conftest.py
@@ -1,13 +1,13 @@
 import pytest
-import requests
+#import requests
 import pandas as pd
 
 # disable any network call during test (even by error)
- @pytest.fixture(autouse=True)
- def disable_network_calls(monkeypatch):
-     def stunted_get():
-         raise RuntimeError("Network access not allowed during testing!")
-     monkeypatch.setattr(requests, "get", lambda *args, **kwargs: stunted_get())
+#  @pytest.fixture(autouse=True)
+#  def disable_network_calls(monkeypatch):
+#      def stunted_get():
+#          raise RuntimeError("Network access not allowed during testing!")
+#      monkeypatch.setattr(requests, "get", lambda *args, **kwargs: stunted_get())
 
 
 pytest_plugins = [
@@ -18,7 +18,7 @@ pytest_plugins = [
 @pytest.fixture(autouse=True)
 def load_sample_data():
     df = pd.read_excel(
-        r"finrecipes\test\datasets\test_termstru_discfact_cubic_spline.xlsx",
+        r"/finrecipes/test/datasets/test_termstru_discfact_cubic_spline.xlsx",
         usecols=[
             "x",
             "y"
