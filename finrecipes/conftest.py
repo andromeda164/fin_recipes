93842606b23e7507c97578b425e86c7a98ecb536
andromeda164 <66296388+andromeda164@users.noreply.github.com>
2021-05-29 07:40:56 +0200

    changed path of conftest

---
+++ finrecipes/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+import requests
+import pandas as pd
+
+# disable any network call during test (even by error)
+ @pytest.fixture(autouse=True)
+ def disable_network_calls(monkeypatch):
+     def stunted_get():
+         raise RuntimeError("Network access not allowed during testing!")
+     monkeypatch.setattr(requests, "get", lambda *args, **kwargs: stunted_get())
+
+
+pytest_plugins = [
+    # add here any fixture plugin
+    #  "tests.unit.fixtures.some_stuff",
+]
+
+@pytest.fixture(autouse=True)
+def load_sample_data():
+    df = pd.read_excel(
+        r"finrecipes\test\datasets\test_termstru_discfact_cubic_spline.xlsx",
+        usecols=[
+            "x",
+            "y"
+        ],
+    )
+
